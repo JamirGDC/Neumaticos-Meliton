4f20f4fe01753d1707b18b1d2501c098df19faee
JamirGDC <gutierrez.dc@icloud.com>
2024-03-29 02:25:10 +0100

    tewt

--- auth.config.ts
+++ auth.config.ts
@@ -8,12 +8,16 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnLogin = nextUrl.pathname.startsWith('/login');
+      if (isOnLogin) {
+        if (isLoggedIn) return Response.redirect(new URL('/dashboard', nextUrl));
+        return false; // Redirect unauthenticated users to login page
+      } 
+      
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
+      } 
       return true;
     },
   },
